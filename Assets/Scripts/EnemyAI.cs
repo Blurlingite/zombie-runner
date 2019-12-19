7b00ff2ce63b97fa168a1f6d10ee357f5d5bfdfb
Vishnu Dawah <vkdawah@gmail.com>
2019-12-19 12:32:10 -0500

    Enemy now only chases player when within range

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -8,19 +8,29 @@ public class EnemyAI : MonoBehaviour
 
 
   [SerializeField] Transform target;
+  // how close to the enemy the player has to be for the enemy to start chasing them
+  [SerializeField] float chaseRange = 5f;
 
   NavMeshAgent navMeshAgent;
-  // Start is called before the first frame update
+  // we want to intialize this as a giant number so the enemy doesn't chase the player right away
+  float distanceToTarget = Mathf.Infinity;
+
   void Start()
   {
     navMeshAgent = GetComponent<NavMeshAgent>();
   }
 
-  // Update is called once per frame
   void Update()
   {
-    // each frame move by setting the destination of the Nav Mesh Agent to be wherever the player currently is
+    // calculate distance from player(the target) to enemy (this object)
+    distanceToTarget = Vector3.Distance(target.position, transform.position);
 
-    navMeshAgent.SetDestination(target.position);
+    // if the distance the player is from enemy is less than the chase range, start chasing the player
+    if (distanceToTarget <= chaseRange)
+    {
+      // each frame move (if in range) by setting the destination of the Nav Mesh Agent to be wherever the player currently is
+      navMeshAgent.SetDestination(target.position);
+    }
+
   }
 }
