4ed9b230c0b7f6c2676cab84df03e203da6cd443
Vishnu Dawah <vkdawah@gmail.com>
2019-12-19 10:42:21 -0500

    Baked Nav Mesh and used Nav Mesh Agent to make Enemy move towards the player

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+
+  [SerializeField] Transform target;
+
+  NavMeshAgent navMeshAgent;
+  // Start is called before the first frame update
+  void Start()
+  {
+    navMeshAgent = GetComponent<NavMeshAgent>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // each frame move by setting the destination of the Nav Mesh Agent to be wherever the player currently is
+
+    navMeshAgent.SetDestination(target.position);
+  }
+}
