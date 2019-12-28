ead121e0f80a0787b67becdcf88d47386fed7f31
Vishnu Dawah <vkdawah@gmail.com>
2019-12-27 20:12:34 -0500

    Worked on enemy attack animation timing

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+  [SerializeField] Transform target;
+  [SerializeField] float damage = 40f;
+
+
+  void Start()
+  {
+
+  }
+
+  // called from our Animation event we placed in the Animation window for the Enemy's attack state
+  public void AttackHitEvent()
+  {
+    if (target == null) return;
+    Debug.Log("bang bang");
+  }
+
+}
