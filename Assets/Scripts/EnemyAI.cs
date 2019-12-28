4b8448341f93c91bbe7edcb38c63530b2d8903d5
Vishnu Dawah <vkdawah@gmail.com>
2019-12-27 21:44:21 -0500

    Enemy now rotates smoothly when engaging the player

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -11,6 +11,7 @@ public class EnemyAI : MonoBehaviour
   [SerializeField] Transform target;
   // how close to the enemy the player has to be for the enemy to start chasing them
   [SerializeField] float chaseRange = 10f;
+  [SerializeField] float turnSpeed = 5f;
 
   NavMeshAgent navMeshAgent;
   // we want to intialize this as a giant number so the enemy doesn't chase the player right away
@@ -44,6 +45,8 @@ public class EnemyAI : MonoBehaviour
 
   private void EngageTarget()
   {
+    // rather than using FaceTarget() in ChaseTarget() & AttackTarget(), we can use it once here. As soon as you engage the target, you shoould be facing the target
+    FaceTarget();
     // stoppingDistance is how far away from it's target the enemy will be once it reaches it's target. So if it is set to 1, the enemy will move towards the target until there is 1 unit of space between it and the target
     // Since distanceToTarget is the space between the player and enemy and as long as it is greater than the stoppingDistance, the enemy will engage the player by moving towards it until it reaches the stoppingDistance
     if (distanceToTarget >= navMeshAgent.stoppingDistance)
@@ -78,7 +81,21 @@ public class EnemyAI : MonoBehaviour
     GetComponent<Animator>().SetBool("attack", true);
   }
 
+  private void FaceTarget()
+  {
 
+    // we need to find the direction, not the distance the player is from the enemy so we use "normalized". That will return a magnitude of 1(instead of what the subtraction below results in) and will also keep the direction 
+    Vector3 direction = (target.position - transform.position).normalized;
+
+    // pass in 0 for y so enemy doesn't rotate up or down
+    Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+    // transform.rotation = where target is, we need to rotate at a certain speed
+    // Quaternion.Slerp() takes in 2 Quaternions and a float(for speed). It will create a smooth rotation between the 2 Quaternions.
+    // transform.rotation is the enemy's current rotation.
+    // lookRotation is the rotation the enemy needs to be to be facing the player, that was calculated above
+    transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+
+  }
 
   // this method listens for when this object (an object with this script) is selected
   void OnDrawGizmosSelected()
