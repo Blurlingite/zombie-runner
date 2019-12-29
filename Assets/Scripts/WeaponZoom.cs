978cbaacacf21af9815bca8a817a9fd7c2401d5f
Vishnu Dawah <vkdawah@gmail.com>
2019-12-29 13:39:00 -0500

    Changed mouse sensitivity depending on if player is zoomed in or out

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
   [SerializeField] Camera fpsCamera;
   [SerializeField] float zoomedOutFOV = 60f; // regular field of vision
   [SerializeField] float zoomedInFOV = 20f; // zoomed field of vision
+  [SerializeField] float zoomOutSensitivity = 2f;
+  [SerializeField] float zoomInSensitivity = 0.5f;
+
 
   bool zoomedInToggle = false;
+  RigidbodyFirstPersonController fpsController;
+
+  void Start()
+  {
+    fpsController = GetComponent<RigidbodyFirstPersonController>();
+  }
 
   void Update()
   {
@@ -19,11 +29,15 @@ public class WeaponZoom : MonoBehaviour
       {
         zoomedInToggle = true;
         fpsCamera.fieldOfView = zoomedInFOV;
+        fpsController.mouseLook.XSensitivity = zoomInSensitivity;
+        fpsController.mouseLook.YSensitivity = zoomInSensitivity;
       }
       else
       {
         zoomedInToggle = false;
         fpsCamera.fieldOfView = zoomedOutFOV;
+        fpsController.mouseLook.XSensitivity = zoomOutSensitivity;
+        fpsController.mouseLook.YSensitivity = zoomOutSensitivity;
       }
     }
   }
