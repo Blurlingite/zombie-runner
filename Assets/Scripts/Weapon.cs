3f758f03351578b6aa624f5590bf52a2cedb808b
Vishnu Dawah <vkdawah@gmail.com>
2019-12-29 19:45:17 -0500

    Implemented ammo system to restrict unlimited firing of weapon

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -12,6 +12,7 @@ public class Weapon : MonoBehaviour
   [SerializeField] float damage = 30f;
   [SerializeField] ParticleSystem muzzleFlash;
   [SerializeField] GameObject hitEffect;
+  [SerializeField] Ammo ammoSlot;
 
   void Update()
   {
@@ -25,8 +26,13 @@ public class Weapon : MonoBehaviour
 
   private void Shoot()
   {
-    PlayMuzzleFlash();
-    ProcessRaycast();
+    if (ammoSlot.GetCurrentAmmo() > 0)
+    {
+      PlayMuzzleFlash();
+      ProcessRaycast();
+      ammoSlot.ReduceCurrentAmmo();
+    }
+
   }
 
   private void PlayMuzzleFlash()
