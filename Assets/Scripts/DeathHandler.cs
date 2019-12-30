6bf59efbf30fbb34e04e6ad18731b3e964123e5a
Vishnu Dawah <vkdawah@gmail.com>
2019-12-30 12:58:20 -0500

    Player can now switch between weapons

--- Assets/Scripts/DeathHandler.cs
+++ Assets/Scripts/DeathHandler.cs
@@ -17,6 +17,8 @@ public class DeathHandler : MonoBehaviour
     gameOverCanvas.enabled = true;
     // freeze time so game doesn't keep playing while Game Over UI is displayed
     Time.timeScale = 0;
+    // disable weapon switching when player is dead
+    FindObjectOfType<WeaponSwitcher>().enabled = false;
     // makes the mouse cursor stop reacting to the game so we can click on things(the equivalent of pressing the ESCAPE key)
     Cursor.lockState = CursorLockMode.None;
     // makes the mouse cursor visible
---
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitcher : MonoBehaviour
+{
+  [SerializeField] int currentWeapon = 0;
+
+  void Start()
+  {
+    SetWeaponActive();
+  }
+
+  void Update()
+  {
+    int previousWeapon = currentWeapon;
+
+    // for keyboard users
+    ProcessKeyInput();
+    // for mouse scrollwheel users, but will throw an exception if there is no scrollwheel on that computer, which is why it is commented out now
+    // ProcessScrollWheel();
+
+    // currentWeapon may change due to the above methods according to user input. If it does, this if statement will execute so we can switch to the weapon the user chose
+    if (previousWeapon != currentWeapon)
+    {
+      SetWeaponActive();
+    }
+  }
+  // only keyboard users can see this method working
+  private void ProcessKeyInput()
+  {
+    // if you press the "1" key
+    if (Input.GetKeyDown(KeyCode.Alpha1))
+    {
+      currentWeapon = 0;
+    }
+
+    if (Input.GetKeyDown(KeyCode.Alpha2))
+    {
+      currentWeapon = 1;
+    }
+
+    if (Input.GetKeyDown(KeyCode.Alpha3))
+    {
+      currentWeapon = 2;
+    }
+  }
+  // only mouse users can see this method working
+  private void ProcessScrollWheel()
+  {
+    // access the axis the scroll wheel belongs to
+    // if you scroll upwards
+    if (Input.GetAxis("Mouse ScollWheel") > 0)
+    {
+      // if we are at the maximum, go back to 0
+      if (currentWeapon >= transform.childCount - 1)
+      {
+        currentWeapon = 0;
+      }
+      else
+      {
+        currentWeapon++;
+      }
+    }
+
+    // if you scroll downwards
+    if (Input.GetAxis("Mouse ScrollWheel") < 0)
+    {
+      // if we are at the maximum, go back to 0
+      if (currentWeapon <= 0)
+      {
+        // return to the weapon with the highest index (3-1 = 2)
+        currentWeapon = transform.childCount - 1;
+      }
+      else
+      {
+        currentWeapon--;
+      }
+    }
+
+  }
+
+
+
+  private void SetWeaponActive()
+  {
+    int weaponIndex = 0;
+
+    // go through each weapon and set active the weapon whose index is equal to currentWeapon
+    // "transform" is the transform of the object this script is attached to
+    foreach (Transform weapon in transform)
+    {
+      if (weaponIndex == currentWeapon)
+      {
+        weapon.gameObject.SetActive(true);
+      }
+      else
+      {
+        weapon.gameObject.SetActive(false);
+      }
+      // if we don't increment, all weapons will be active
+      weaponIndex++;
+    }
+
+  }
+}
