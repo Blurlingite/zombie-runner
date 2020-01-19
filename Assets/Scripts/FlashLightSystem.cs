09a5a0855fda8a7910519196fa764202f0f1c82c
Vishnu Dawah <vkdawah@gmail.com>
2020-01-19 14:51:30 -0500

    Added a flashlight that loses intensity and angle as more time passes

---
+++ Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+  [SerializeField] float lightDecay = 0.1f;
+  [SerializeField] float angleDecay = 1.0f;
+  // so the light doesn't get too small
+  [SerializeField] float minimumAngle = 40f;
+
+  Light myLight;
+
+
+  void Start()
+  {
+    myLight = GetComponent<Light>();
+  }
+
+  void Update()
+  {
+    DecreaseLightAngle();
+    DecreaseLightIntensity();
+
+  }
+
+  private void DecreaseLightAngle()
+  {
+    if (myLight.spotAngle <= minimumAngle)
+    {
+      return;
+    }
+    else
+    {
+      myLight.spotAngle -= angleDecay * Time.deltaTime;
+    }
+  }
+
+  private void DecreaseLightIntensity()
+  {
+    myLight.intensity -= lightDecay * Time.deltaTime;
+  }
+}
