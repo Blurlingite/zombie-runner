e27665471dff39b6f3d4a4f601f53ed8e3d444e6
Vishnu Dawah <vkdawah@gmail.com>
2020-02-20 19:14:19 -0500

    Added Weapons Scene

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,26 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour
-{
-    public void ReloadScene()
-    {
-        SceneManager.LoadScene(1);
+public class SceneLoader : MonoBehaviour {
+    public void ReloadScene () {
+        SceneManager.LoadScene (1);
         Time.timeScale = 1;
     }
 
-    public void StartGame()
-    {
-        SceneManager.LoadScene(1);
+    public void StartGame () {
+        SceneManager.LoadScene (1);
     }
 
-    public void SeeControls()
-    {
-        SceneManager.LoadScene(3);
+    public void HomeScene () {
+        SceneManager.LoadScene (2);
     }
 
-    public void QuitGame()
-    {
-        Application.Quit();
+    public void SeeControls () {
+        SceneManager.LoadScene (3);
+    }
+
+    public void WeaponsScene () {
+        SceneManager.LoadScene (4);
+    }
+
+    public void QuitGame () {
+        Application.Quit ();
     }
 }
