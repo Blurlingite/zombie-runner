f2f5795bd49c1d9edca811537b06ed34df13aef4
Vishnu Dawah <vkdawah@gmail.com>
2020-02-23 19:53:17 -0500

    Added Pickups Scene

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,29 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour {
-    public void ReloadScene () {
-        SceneManager.LoadScene (1);
+public class SceneLoader : MonoBehaviour
+{
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(1);
         Time.timeScale = 1;
     }
 
-    public void StartGame () {
-        SceneManager.LoadScene (1);
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
     }
 
-    public void HomeScene () {
-        SceneManager.LoadScene (2);
+    public void HomeScene()
+    {
+        SceneManager.LoadScene(2);
     }
 
-    public void SeeControls () {
-        SceneManager.LoadScene (3);
+    public void PickupsScene()
+    {
+        SceneManager.LoadScene(5);
     }
 
-    public void WeaponsScene () {
-        SceneManager.LoadScene (4);
+    public void SeeControls()
+    {
+        SceneManager.LoadScene(3);
     }
 
-    public void QuitGame () {
-        Application.Quit ();
+    public void WeaponsScene()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
