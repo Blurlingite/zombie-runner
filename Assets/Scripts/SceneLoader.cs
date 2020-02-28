aac9d51dfa98a6a3384cc89fe1484f9a0b959aa0
Vishnu Dawah <vkdawah@gmail.com>
2020-02-27 20:23:46 -0500

    Added enemies, pickups and changed world lighting

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,41 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour
-{
-    public void ReloadScene()
-    {
-        SceneManager.LoadScene(1);
+public class SceneLoader : MonoBehaviour {
+    public void ReloadScene () {
+        SceneManager.LoadScene (4);
         Time.timeScale = 1;
     }
 
-    public void StartGame()
-    {
-        SceneManager.LoadScene(1);
+    public void StartGame () {
+        SceneManager.LoadScene (4);
     }
 
-    public void HomeScene()
-    {
-        SceneManager.LoadScene(2);
+    public void HomeScene () {
+        SceneManager.LoadScene (0);
     }
 
-    public void PickupsScene()
-    {
-        SceneManager.LoadScene(5);
+    public void PickupsScene () {
+        SceneManager.LoadScene (3);
     }
 
-    public void SeeControls()
-    {
-        SceneManager.LoadScene(3);
+    public void SeeControls () {
+        SceneManager.LoadScene (1);
     }
 
-    public void WeaponsScene()
-    {
-        SceneManager.LoadScene(4);
+    public void WeaponsScene () {
+        SceneManager.LoadScene (2);
     }
 
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
+    // public void QuitGame () {
+    //     Application.Quit ();
+    // }
 }
