8b06590e8184a4a146be10786c65f50f8c2fa655
Vishnu Dawah <vkdawah@gmail.com>
2020-03-19 17:42:12 -0400

    You can now use the Escape key to escape from fullscreen mode

---
+++ Assets/Scripts/FullScreenToggle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullScreenToggle : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Screen.fullScreen && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Screen.fullScreenMode = FullScreenMode.Windowed;
+        }
+
+    }
+}
